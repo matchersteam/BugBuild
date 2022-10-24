848349c9a56f0ec252fd3b5f445db4cd22e75dce
Alex Galey <alex@matchers.fr>
2022-10-24 22:10:21 +0200

    Reproduce versions and fix IS import

--- vue-instantsearch-vue3/vite.config.js
+++ vue-instantsearch-vue3/vite.config.js
@@ -1,8 +1,43 @@
+import path from 'path'
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  base: '/',
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`
+    }
+  },
+  plugins: [vue(),
+  //   AutoImport({
+  //     imports: [
+  //       'vue',
+  //       'vue-router'
+  //     ],
+  //     vueTemplate: true
+  //   })
+  ],
+  // optimizeDeps: {
+  //   include: [
+  //     'vue',
+  //     'vue-router',
+  //     '@vueuse/core',
+  //     '@vueuse/head'
+  //   ],
+  //   exclude: [
+  //     'vue-demi',
+  //     'vue-instantsearch'
+  //   ]
+  // }
+  // ssr: {
+  //   // TODO: workaround until they support native ESM
+  //   noExternal: ['vue-instantsearch']
+  // },
+  // ssgOptions: {
+  //   script: 'async',
+  //   formatting: 'minify'
+  // }
 });
