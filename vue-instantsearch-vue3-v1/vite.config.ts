f760d26813e6f212294f0bdf50cd542b82700e10
alex <guy-alexandre.eclapier@epitech.eu>
2022-11-02 18:34:02 +0100

    vite.config with esmodule

--- vue-instantsearch-vue3-v1/vite.config.ts
+++ vue-instantsearch-vue3-v1/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-
+import esmodule from 'vite-plugin-esmodule'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+plugins: [vue()
+, esmodule([
+'vue-instantsearch/vue3/es/',
+{'file-type': 'vue-instantsearch/vue3/es/index.js'}
+])
+],
+// ssgOptions: {
+// format: 'cjs'
+// }
 });
